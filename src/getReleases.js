105d39e9c3fabdb2daa66145de231da333a71a1d
Austin Godber <godber@uberhip.com>
2020-07-23 15:34:22 -0700

    make auth header option based on github token

--- src/getReleases.js
+++ src/getReleases.js
@@ -6,12 +6,17 @@ const { GITHUB_TOKEN } = process.env;
 export default async function getReleases(user, repo) {
   const url = `https://api.github.com/repos/${user}/${repo}/releases`;
 
-  const r = await got.get(url, {
+  const requestConfig = {
     headers: {
-      'User-Agent': name,
-      Authorization: `token ${GITHUB_TOKEN}`,
+      'User-Agent': name
     },
     responseType: 'json'
-  });
+  };
+
+  if (GITHUB_TOKEN) {
+    requestConfig.headers.Authorization = `token ${GITHUB_TOKEN}`;
+  }
+
+  const r = await got.get(url, requestConfig);
   return r.body;
 }
